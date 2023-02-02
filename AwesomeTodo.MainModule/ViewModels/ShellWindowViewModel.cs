92b1f07f887c99ca07204cccf087d4c2f59053dd
johanrdev <johanrothman1988@gmail.com>
2023-02-02 20:26:28 +0100

    Hooked up the commands for navigation buttons

--- AwesomeTodo.MainModule/ViewModels/ShellWindowViewModel.cs
+++ AwesomeTodo.MainModule/ViewModels/ShellWindowViewModel.cs
@@ -1,8 +1,28 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Diagnostics;
 
 namespace AwesomeTodo.MainModule.ViewModels
 {
     internal class ShellWindowViewModel : BindableBase
     {
+        public DelegateCommand GotoPreviousViewCommand { get; }
+        public DelegateCommand GotoNextViewCommand { get; }
+
+        public ShellWindowViewModel()
+        {
+            GotoPreviousViewCommand = new DelegateCommand(ExecuteGotoPreviousViewCommand);
+            GotoNextViewCommand = new DelegateCommand(ExecuteGotoNextViewCommand);
+        }
+
+        private void ExecuteGotoPreviousViewCommand()
+        {
+            Debug.WriteLine("Prev");
+        }
+
+        private void ExecuteGotoNextViewCommand()
+        {
+            Debug.WriteLine("Next");
+        }
     }
 }
