ffb589e6f9beb7c1adfbe13b161a1d0c425022d7
johanrdev <johanrothman1988@gmail.com>
2023-02-02 21:44:23 +0100

    Added custom font and bound listview itemssource to dummy data

--- AwesomeTodo.MainModule/ViewModels/ShellWindowViewModel.cs
+++ AwesomeTodo.MainModule/ViewModels/ShellWindowViewModel.cs
@@ -1,18 +1,35 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 
 namespace AwesomeTodo.MainModule.ViewModels
 {
     internal class ShellWindowViewModel : BindableBase
     {
+        private Todo _selectedTodo;
+        
+        public Todo SelectedTodo
+        {
+            get => _selectedTodo;
+            set => SetProperty(ref _selectedTodo, value);
+        }
+
+        public string Title => "AwesomeTodo";
         public DelegateCommand GotoPreviousViewCommand { get; }
         public DelegateCommand GotoNextViewCommand { get; }
+        public ObservableCollection<Todo> Todos { get; }
 
         public ShellWindowViewModel()
         {
             GotoPreviousViewCommand = new DelegateCommand(ExecuteGotoPreviousViewCommand);
             GotoNextViewCommand = new DelegateCommand(ExecuteGotoNextViewCommand);
+            Todos = new ObservableCollection<Todo>();
+            Todos.Add(new Todo { Title = "My first todo" });
+            Todos.Add(new Todo { Title = "My second todo" });
+            Todos.Add(new Todo { Title = "My third todo" });
+            Todos.Add(new Todo { Title = "My fourth todo" });
+            Todos.Add(new Todo { Title = "My fifth todo" });
         }
 
         private void ExecuteGotoPreviousViewCommand()
@@ -23,6 +40,24 @@ namespace AwesomeTodo.MainModule.ViewModels
         private void ExecuteGotoNextViewCommand()
         {
             Debug.WriteLine("Next");
+        }
+    }
+
+    public class Todo : BindableBase
+    {
+        private int _id;
+        private string _title;
+        
+        public int Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
         }
     }
 }
