881b3c63a1211b487428e2fdc89f17f080d2306d
johanrdev <johanrothman1988@gmail.com>
2023-02-02 20:19:19 +0100

    Created the prev/next navigation buttons as a user control

---
+++ AwesomeTodo.Shared/Controls/NavigationButtons.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AwesomeTodo.Shared.Controls
+{
+    /// <summary>
+    /// Interaction logic for NavigationButtons.xaml
+    /// </summary>
+    public partial class NavigationButtons : UserControl
+    {
+        public NavigationButtons()
+        {
+            InitializeComponent();
+        }
+    }
+}
