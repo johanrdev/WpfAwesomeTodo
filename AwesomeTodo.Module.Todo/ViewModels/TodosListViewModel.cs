dbe8dca02325dee05dee290239cc2176437c0d7a
johanrdev <johanrothman1988@gmail.com>
2023-02-03 18:29:39 +0100

    Hooked up commands for todoslistviewmodel

--- AwesomeTodo.Module.Todo/ViewModels/TodosListViewModel.cs
+++ AwesomeTodo.Module.Todo/ViewModels/TodosListViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using AwesomeTodo.DataAccess.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 
 namespace AwesomeTodo.Module.Todo.ViewModels
@@ -16,6 +18,9 @@ namespace AwesomeTodo.Module.Todo.ViewModels
         }
 
         public ObservableCollection<TodoItem> Todos { get; }
+        public DelegateCommand GotoAddTodoViewCommand { get; }
+        public DelegateCommand<object> RemoveTodosCommand { get; }
+        public DelegateCommand ResetTodosCommand { get; }
 
         public TodosListViewModel()
         {
@@ -26,6 +31,41 @@ namespace AwesomeTodo.Module.Todo.ViewModels
             Todos.Add(new TodoItem { Title = "My fourth todo" });
             Todos.Add(new TodoItem { Title = "My fifth todo" });
             SelectedTodo = Todos.First();
+
+            GotoAddTodoViewCommand = new DelegateCommand(ExecuteGotoAddTodoViewCommand, CanExecuteGotoAddTodoViewCommand);
+            RemoveTodosCommand = new DelegateCommand<object>(ExecuteRemoveTodosCommand, CanExecuteRemoveTodosCommand);
+            ResetTodosCommand = new DelegateCommand(ExecuteResetTodosCommand, CanExecuteResetTodosCommand);
+        }
+
+        private void ExecuteGotoAddTodoViewCommand()
+        {
+            Debug.WriteLine("Goto add todo view");
+        }
+
+        private bool CanExecuteGotoAddTodoViewCommand()
+        {
+            return true;
+        }
+
+        private void ExecuteRemoveTodosCommand(object obj)
+        {
+            Debug.WriteLine("Remove todos:");
+            Debug.WriteLine(obj);
+        }
+
+        private bool CanExecuteRemoveTodosCommand(object obj)
+        {
+            return true;
+        }
+
+        private void ExecuteResetTodosCommand()
+        {
+            Debug.WriteLine("Reset todos");
+        }
+
+        private bool CanExecuteResetTodosCommand()
+        {
+            return true;
         }
     }
 }
